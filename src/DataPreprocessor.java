8aa7b64932b6b596565ed0ce449a306df5d54fda
Kalhan <kalhan@mbp-de-kalhan.3521.dhcp.asu.edu>
2023-04-08 16:47:22 -0700

    minor edit

--- src/DataPreprocessor.java
+++ src/DataPreprocessor.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class DataPreprocessor {
+
+
+    public Float getTemperature() {
+        //read from file
+        return null;
+    }
+
+
+
+    public Float getGasConcentraion() {
+        //read from file
+        return null;
+    }
+
+    public Float getPM25(){
+        //read from file
+        return null;
+    }
+
+
+
 }
