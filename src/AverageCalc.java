c9cdb9a8300273378055763e2d214bac0c3be365
spanchak <spanchak@asu.edu>
2023-04-08 20:05:05 -0700

    DataSmoothener Component V1

---
+++ src/AverageCalc.java
@@ -0,0 +1,35 @@
+public class AverageCalc{
+
+    private float total;
+    private int len_array;
+    private float[] array;
+    private int window_size;
+
+    public AverageCalc(int window_size){
+        this.window_size = window_size;
+        this.array = new float[window_size];
+        this.total = 0;
+        this.len_array = 0;
+    }
+
+    public float CalculateAvg(float input) {
+        total = total - array[len_array];
+        total = total + input;
+        float output = total / len_array;
+        UpdateArr(input);
+        CheckArr();
+		return output;
+	}
+
+	public void CheckArr() {
+		if (len_array >= window_size){
+            len_array = 0;
+            }
+	}
+
+	public void UpdateArr(float input) {
+		array[len_array] = input;
+        len_array = len_array + 1;
+	}
+
+}
--- src/DataSmoothener.java
+++ src/DataSmoothener.java
@@ -1,5 +1,42 @@
 public class DataSmoothener {
-    private float total = 0;
-    private int len_array = 0;
-    private float[] array = new float[5];
+    int window_size;
+    AverageCalc AverageCalc_Temp = new AverageCalc(5);
+    AverageCalc AverageCalc_Hum = new AverageCalc(5);
+    AverageCalc AverageCalc_CO = new AverageCalc(5);
+    AverageCalc AverageCalc_CO2 = new AverageCalc(5);
+    AverageCalc AverageCalc_NO2 = new AverageCalc(5);
+    AverageCalc AverageCalc_PM25 = new AverageCalc(5);
+    AverageCalc AverageCalc_PM10 = new AverageCalc(5);
+
+    public DataSmoothener(int window_size){
+        this.window_size = window_size;
+    }
+
+    public void AvgCalcTemp(float in){
+        float output = AverageCalc_Temp.CalculateAvg(in);
+    }
+
+    public void AvgCalcHum(float in){
+        float output = AverageCalc_Hum.CalculateAvg(in);
+    }
+
+    public void AvgCalcCO(float in){
+        float output = AverageCalc_CO.CalculateAvg(in);
+    }
+
+    public void AvgCalcCO2(float in){
+        float output = AverageCalc_CO2.CalculateAvg(in);
+    }
+
+    public void AvgCalcNO2(float in){
+        float output = AverageCalc_NO2.CalculateAvg(in);
+    }
+
+    public void AvgCalcPM25(float in){
+        float output = AverageCalc_PM25.CalculateAvg(in);
+    }
+
+    public void AvgCalcPM10(float in){
+        float output = AverageCalc_PM10.CalculateAvg(in);
+    }
 }
