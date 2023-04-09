404db1a4d78c28459cb44ec4f78ede959e5a7bbc
spanchak <spanchak@asu.edu>
2023-04-08 19:06:14 -0700

    Changes related to config.properties

--- src/DataSmoothener.java
+++ src/DataSmoothener.java
@@ -1,3 +1,5 @@
 public class DataSmoothener {
-
+    private float total = 0;
+    private int len_array = 0;
+    private float[] array = new float[5];
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.io.*;
+import java.util.Properties;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Running air quality monitoring project");
-
-
+        System.out.println("Reading Config File:...");
+        try {
+            String configFilePath = "./config.properties";
+            FileInputStream propsInput = new FileInputStream(configFilePath);
+            Properties prop = new Properties();
+            prop.load(propsInput);
+            System.out.println(prop.getProperty("window_size"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
